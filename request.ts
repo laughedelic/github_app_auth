cb63735485eeb4788566d4233a88ad4137baa43c
Alexey Alekhin <alexey.alekhin@tapad.com>
2021-04-05 03:20:06 +0200

    feat: requests on behalf of an app

---
+++ request.ts
@@ -0,0 +1,34 @@
+const githubApiUrl = "https://api.github.com";
+
+export async function appRequest(
+  jwt: string,
+  endpoint: string,
+  params?: Record<string, any>
+) {
+  const response = await fetch(`${githubApiUrl}/${endpoint}`, {
+    headers: {
+      authorization: `bearer ${jwt}`,
+      accept: "application/vnd.github.v3+json",
+    },
+    ...params,
+  });
+  return await response.json();
+}
+
+export function listInstallations(jwt: string) {
+  return appRequest(jwt, "app/installations");
+}
+
+export function createInstallationToken(
+  jwt: string,
+  installationId: string,
+  ...repositories: string[]
+) {
+  const body = {
+    repositories,
+  };
+  return appRequest(jwt, `app/installations/${installationId}/access_tokens`, {
+    method: "POST",
+    body: JSON.stringify(body),
+  });
+}
