378d8d6f3aa0973636d4115e716a7ce6cabf63bc
Alexey Alekhin <alexey.alekhin@tapad.com>
2021-04-05 03:19:48 +0200

    feat: app JWT function

---
+++ jwt.ts
@@ -0,0 +1,13 @@
+import { create, getNumericDate } from "https://deno.land/x/djwt/mod.ts";
+
+export function appJwt(appId: string, privateKey: string): Promise<string> {
+  return create(
+    { alg: "RS256", typ: "JWT" },
+    {
+      iss: appId, // issuer
+      iat: getNumericDate(0), // issued at time (now)
+      exp: getNumericDate(5 * 60), // expiration time (in 5 minutes)
+    },
+    atob(privateKey)
+  );
+}
